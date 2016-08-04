f5047803ea06e84cca6fd0581f24df2711fda8c7
Rodrigo Castro <rfocastro@sp.r7.com>
2016-08-04 19:32:15 -0300

    estrutura inicial do projeto

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var  express = require('express'),
      load = require('express-load'),
-     routes = require('./routes');
-
-var app = express();
+     app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes.index);
-app.get('/usarios', routes.user.index);
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
+
 
 app.listen(3000, function(){
   console.log('Ntalk no ar');
